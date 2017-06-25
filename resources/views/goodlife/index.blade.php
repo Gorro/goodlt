53d7162a50951f583e27505898848b62a8d58f98
Gorro <afastudillo@ing.ucsc.cl>
2017-06-25 00:33:41 -0400

    avance goodlifetatto

---
+++ resources/views/goodlife/index.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>GoodLifeTattoo</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="{{url('')}}/css/bootstrap.css">
+    <!--Fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Alfa+Slab+One" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+</head>
+<body>
+    <div class="head">       
+        <h1 class="title yelow large text-center top">GOOD LIFE TATTO</h1>
+        <h2 class="title white medium text-center">ESTUDIO PRIVADO</h2>
+    </div>
+    <div class="resena">
+
+    </div>
+    <div class="col-md-6">
+        <img src="{{url('')}}/images/tatuaje-trais.jpg" style="height: 100vh;" alt="">
+    </div>
+    <div class="col-md-6">
+        <div class="centra">
+            <h2 class="title yelow medium text-center bottom">RESEÑA</h2>
+            <p style="text-align: justify;">Cristian Pérez Galaz,&nbsp; artista tatuador con varios años de experiencia, comenzó su vida siempre ligado al dibujo, ganando varios concursos escolares y a nivel de comuna.</p>
+            <p style="text-align: justify;">En su adolescencia, atraído por el anonimato y la rebeldía dedica su tiempo al street art (graffiti y stencil), ahí conoce entre sus contactos el mundo del tatuaje. Con estudios técnicos de diseño e ilustración comienza a tatuar en forma esporádica a amigos y conocidos, siempre con entrega en su trabajo y mostrando ganas de aprender y crecer como tatuador.</p>
+            <p style="text-align: justify;">Es en el 2014 cuando volcó su creatividad al arte del tatuaje, pasando por estudios comerciales y&nbsp; captando clientes a través de un trabajo responsable, higiénico y bien realizado.</p>
+            <p style="text-align: justify;">Hoy atiende en su estudio privado, con horas previamente agendadas a personas que busquen un tatuaje artístico de alta calidad.</p>
+
+        </div>
+    </div>
+</body>
+</html>
