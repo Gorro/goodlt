eb6f54c36fbf50428a5519e528fcc6c88a4206ff
Gorro <afastudillo@ing.ucsc.cl>
2017-06-25 02:58:54 -0400

    hmtl terminado, falta funcionalidad

--- resources/views/goodlife/index.blade.php
+++ resources/views/goodlife/index.blade.php
@@ -10,7 +10,16 @@
     <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
 </head>
 <body>
-    <div class="head">       
+    <div class="head">
+        <div class="menu">
+            <ul class="center-block title">
+                <li class="logo"><img src="{{url('')}}/images/logo-good-life-tattoo-150x150.png" alt=""></li>
+                <li>INICIO |</li>
+                <li>RESEÑA |</li>
+                <li>GALERÍA |</li>
+                <li>CONTACTO</li>
+            </ul>
+        </div>       
         <h1 class="title yelow large text-center top">GOOD LIFE TATTO</h1>
         <h2 class="title white medium text-center">ESTUDIO PRIVADO</h2>
     </div>
@@ -18,7 +27,7 @@
 
     </div>
     <div class="col-md-6">
-        <img src="{{url('')}}/images/tatuaje-trais.jpg" style="height: 100vh;" alt="">
+        <img src="{{url('')}}/images/tatuaje-trais.jpg" class="full" style="height: 100vh;" alt="">
     </div>
     <div class="col-md-6">
         <div class="centra">
@@ -27,7 +36,52 @@
             <p style="text-align: justify;">En su adolescencia, atraído por el anonimato y la rebeldía dedica su tiempo al street art (graffiti y stencil), ahí conoce entre sus contactos el mundo del tatuaje. Con estudios técnicos de diseño e ilustración comienza a tatuar en forma esporádica a amigos y conocidos, siempre con entrega en su trabajo y mostrando ganas de aprender y crecer como tatuador.</p>
             <p style="text-align: justify;">Es en el 2014 cuando volcó su creatividad al arte del tatuaje, pasando por estudios comerciales y&nbsp; captando clientes a través de un trabajo responsable, higiénico y bien realizado.</p>
             <p style="text-align: justify;">Hoy atiende en su estudio privado, con horas previamente agendadas a personas que busquen un tatuaje artístico de alta calidad.</p>
-
+        </div>
+    </div>
+    <div class="col-md-12">
+        <h2 class="title yelow medium text-center bottom" style="margin-top:30px;">GALERÍA</h2>
+        <div class="container">
+            <div class="col-md-3 hover07"><img src="{{url('')}}/galeria/pecho.jpg" class="galeria full"></div>
+            <div class="col-md-3 hover07"><img src="{{url('')}}/galeria/hanamichi.jpg" class="galeria full"></div>
+            <div class="col-md-3 hover07"><img src="{{url('')}}/galeria/hombro.jpg" class="galeria full"></div>
+            <div class="col-md-3 hover07"><img src="{{url('')}}/galeria/brazo.jpg" class="galeria full"></div>
+            <div class="clearfix"></div>
+            <br>
+            <div class="col-md-3 hover07"><img src="{{url('')}}/galeria/pecho2.jpg" class="galeria full"></div>
+            <div class="col-md-3 hover07"><img src="{{url('')}}/galeria/hombro2.jpg" class="galeria full"></div>
+            <div class="col-md-3 hover07"><img src="{{url('')}}/galeria/brazo2.jpg" class="galeria full"></div>
+        </div>
+    </div>
+    <div class="clearfix"></div>
+    <div class="contacto centra container">
+        <h2 class="title yelow medium text-center bottom">CONTACTO</h2>
+        <div class="col-md-3">
+            <div class="col-xs-12 centra2">
+                <img src="{{url('')}}/images/telefono.png" class="center-block" alt="">
+                <p class="text-center">+56 9 51944918</p>
+            </div>
+            <div class="col-xs-12 centra2">
+                <img src="{{url('')}}/images/email.png" class="center-block" alt="">
+                <p class="text-center">contacto@goodlifetatto.cl</p>
+            </div>
+            <div class="col-xs-12 centra2">
+                <img src="{{url('')}}/images/message.png" class="center-block" alt="">
+                <p class="text-center">/cristian.pereztattooist</p>
+            </div>
+        </div>
+        <div class="col-md-9">
+            <form>
+            <div class="form-group">
+                <input type="email" class="form-control" id="email" placeholder="Ingresa tu nombre">
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control" id="password" placeholder="Ingresa tu email">
+            </div>
+            <div class="form-group">
+                <textarea name="" id="" cols="30" rows="10" class="form-control" placeholder="Escribe el mensaje"></textarea>
+            </div>            
+            <center><button type="submit" class="btn btn-warning">Enviar</button></center>
+            </form>
         </div>
     </div>
 </body>
